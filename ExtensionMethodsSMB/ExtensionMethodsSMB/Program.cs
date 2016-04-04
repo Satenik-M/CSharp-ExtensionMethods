2685bb378c2f6830ea7b3a5cee0edbd5423b37cb
Satenik-M <satenik@markosyan.am>
2016-04-04 14:07:10 +0400

    backend homework for 04/04/2016

---
+++ ExtensionMethodsSMB/ExtensionMethodsSMB/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsSMB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6,7 };
+            array.ToScreen();
+            Console.WriteLine("the new array with even memebers only");
+            array.EvenElements().ToScreen();
+            Console.ReadLine();
+
+        }
+    }
+}
